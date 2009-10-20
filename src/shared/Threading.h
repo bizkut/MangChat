d2569660c694d76ff4dab644bd7466d8502c5d59
Xeross <x3r0ss@gmail.com>
2009-10-20 11:46:44 +0200

    * Added a crashfix in the threading code

--- src/shared/Threading.h
+++ src/shared/Threading.h
@@ -36,11 +36,16 @@ namespace ACE_Based
             void incReference() { ++m_refs; }
             void decReference()
             {
-                if(!--m_refs)
+                if(!--m_refs && !m_deleted)
+                {
+                    m_deleted = true;
                     delete this;
+                }
             }
         private:
             ACE_Atomic_Op<ACE_Thread_Mutex, int> m_refs;
+
+            bool m_deleted;
     };
 
     enum Priority
