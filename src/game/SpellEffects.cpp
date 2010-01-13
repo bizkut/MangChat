3aa476c922ba693c485b3c0671212e3e1b80df23
3raZar3 <erazare@clanice.com>
2010-01-13 16:39:35 -0500

    Mangchat [9170] rebase v032

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -5182,6 +5182,7 @@ void Spell::EffectScriptEffect(uint32 effIndex)
 
                     break;
                 }
+
                 // High Executor's Branding Iron
                 case 48603:
                     // Torture the Torturer: High Executor's Branding Iron Impact
